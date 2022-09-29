96389a2ad830376065787c7c81ceab37c7d3242d
adharwaj <adharwaj@INLEN8520046589.corp.capgemini.com>
2022-09-29 16:26:44 +0530

    this is first git eclipse project to be pushed

---
+++ src/main/java/com/basic/Addition.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers");
+		int first = 25;
+		int second = 45;
+		
+		System.out.println(first + " "+ second);
+		
+		int sum = first+second;
+		
+		System.out.println("The sum is: "+ sum);
+
+	}
+
+}
